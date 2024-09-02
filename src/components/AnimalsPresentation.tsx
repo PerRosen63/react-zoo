c58f0b75bda76e914f76df84d4100e5e347bb66e
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-09-02 14:36:28 +0200

    ✨ placeholder image for broken link

--- src/components/AnimalsPresentation.tsx
+++ src/components/AnimalsPresentation.tsx
@@ -1,21 +1,37 @@
 import { Link } from "react-router-dom";
 import { IAnimal } from "../models/IAnimal";
+import placeholderImage from "../assets/placeholder.png";
 
 interface IAnimalsPresentationProps {
-    animals: IAnimal[];
+  animals: IAnimal[];
 }
 
 export const AnimalsPresentation = ({ animals }: IAnimalsPresentationProps) => {
-
-    return <>
-    <section className="animals">
+  return (
+    <>
+      <section className="animals">
         {animals.map((animal) => (
-            <div key={animal.id} className="animal">
-                <div><img width="300" src={animal.imageUrl} alt={animal.name} /></div>
-                <div><h3>{animal.name}</h3></div>
-                <div><Link to={`/animal/${animal.id}`}>Lär mer</Link></div>
+          <div key={animal.id} className="animal">
+            <div>
+              <img
+                width="300"
+                src={animal.imageUrl}
+                alt={animal.name}
+                onError={(e) => {
+                  e.currentTarget.onerror = null;
+                  e.currentTarget.src = placeholderImage;
+                }}
+              />
             </div>
+            <div>
+              <h3>{animal.name}</h3>
+            </div>
+            <div>
+              <Link to={`/animal/${animal.id}`}>Läs mer</Link>
+            </div>
+          </div>
         ))}
-    </section>
+      </section>
     </>
-}
+  );
+};
