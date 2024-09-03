78e692b6ee6fa4de33f4d4394a69005f4508a8fa
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-09-03 06:08:39 +0200

    ✨ Homepage content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 export const Home = () => {
-    return<>
-    Home
+  return (
+    <>
+      <section className="home">
+        <h1>Hjälp Zooparken att mata djuren!</h1>
+        <div>
+          <Link to={`/animals`}>Möt våra djur!</Link>
+        </div>
+      </section>
     </>
-}
+  );
+};
